25f4f05bc01972dd39693c2c1aa80c19e4629148
Minwoo Seon <mwseon@cs.utexas.edu>
2022-09-29 16:43:41 -0500

    initial

---
+++ main_client.py
@@ -0,0 +1,13 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from subprocess import run, PIPE
+
+
+class Client:
+    def __inti__(self, attr1, attr2, attr3):
+        pass
+    
+    def function1(self):
+        pass
+
+if __name__ = '__main__':
+    pass
