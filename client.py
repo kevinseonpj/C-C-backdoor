cf71013eb355aaf7daf5e104c1f263df1d19a9c1
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2022-10-05 20:18:42 -0500

    Add port argument

--- client.py
+++ client.py
@@ -4,11 +4,11 @@ import sys
 # this is for the attacking machine
 
 HOST = sys.argv[1] 
-PORT = 1111
+PORT = sys.argv[2]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-pwd = sys.argv[2]
+pwd = sys.argv[3]
 s.send(pwd.encode())
 data = s.recv(1024).decode()
 if data != "done":
