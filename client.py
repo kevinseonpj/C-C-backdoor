700abd20db43921829d727ac3381d57c52024efd
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2022-10-05 20:43:30 -0500

    Add print statements

--- client.py
+++ client.py
@@ -7,12 +7,15 @@ HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Created socket")
 s.connect((HOST, PORT))
+print("Connected to socket")
 pwd = sys.argv[3]
 s.send(pwd.encode())
 data = s.recv(1024).decode()
+print("Received data: " + data)
 if data != "done":
     print(data)
     exit()
-
+print("Finished")
 s.close()
